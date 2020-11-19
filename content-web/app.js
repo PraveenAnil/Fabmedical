63bc2f1ba1fccc51a0586681862bfcb65ef0b453
PraveenAnil <praveendosth@gmail.com>
2020-11-19 19:44:13 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+appInsights.setup("ed755e62-c691-4474-9e93-411b1e7807be");
+appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://13.82.151.179:3001";
